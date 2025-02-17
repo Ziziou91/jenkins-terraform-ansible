c6b640d61be9a8e3858d1ec88f7a2e10195e9321
Joshua Gray <joshgray91@gmail.com>
2025-02-17 20:17:05 +0000

    implement node_exporter setup on ec2 startup

--- jenkins_setup.sh
+++ jenkins_setup.sh
@@ -4,3 +4,33 @@ yum update -y
 
 # Install Python
 yum install -y python3 python3-pip
+
+
+# Create user monitoring with no password
+sudo su
+useradd -m -s /bin/bash monitoring
+cd /home/monitoring
+
+
+# Download the latest node-exporter tar file from official documentation
+wget https://github.com/prometheus/node_exporter/releases/download/v1.2.2/node_exporter-1.2.2.linux-amd64.tar.gz
+tar -xzvf node_exporter-1.2.2.linux-amd64.tar.gz
+mv node_exporter-1.2.2.linux-amd64 /home/monitoring/node_exporter
+chown -R monitoring:monitoring /home/monitoring/node_exporter
+
+# Add file contents into service file
+cat <<EOT >> /etc/systemd/system/node_exporter.service
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+[Service]
+User=monitoring
+ExecStart=/home/monitoring/node_exporter/node_exporter
+[Install]
+WantedBy=default.target
+EOT
+
+systemctl daemon-reload
+systemctl start node_exporter
+systemctl enable node_exporter
